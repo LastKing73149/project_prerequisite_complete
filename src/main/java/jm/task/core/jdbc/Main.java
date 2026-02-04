68c54964585516d42099df4aecebd22dcb49c013
LastKing73149 <vladuskim9359@gmail.com>
2026-02-04 16:03:59 +0900

    Complete the task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // implement algorithm here
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Billy", "Herington", (byte)32);
+        userService.saveUser("Van", "Darkholm", (byte) 35);
+        userService.saveUser("Uncle", "Bogdan", (byte) 40);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // set up a database connection
+        private static final String url = "jdbc:mysql://localhost:3306/example_schema";
+        private static final String username = "root";
+        private static final String password = "1234567890D";
+        private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+        public static Connection getConnection () {
+            Connection connection = null;
+            try {
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
 }
