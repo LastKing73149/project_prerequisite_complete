fdcb70a5ea32a50c22413de3f37f7ecc4bc3d4c3
Vladislav Kim <vladuskim9359@gmail.com>
2026-02-07 14:48:39 +0900

    Main updated

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
@@ -13,12 +14,12 @@ public class Main {
         User user1 = new User("Billy", "Genie", (byte)32);
         User user2 = new User("Van", "Kachalkin", (byte) 35);
         User user3 = new User("Uncle", "Bogdan", (byte) 40);
-        User user4 = new User("Johnny", "Makarony", (byte) 20)
+        User user4 = new User("Johnny", "Makarony", (byte) 20);
 
         User[] users = {user1, user2, user3, user4};
         for(User user : users){
             userService.saveUser(user.getName(), user.getLastName(), user.getAge());
-            System.out.println("Добавлен пользователь с именем - " + user.getName() + "в базу данных");
+            System.out.println("Добавлен пользователь с именем - " + user.getName() + " в базу данных");
         }
 
         userService.getAllUsers().forEach(System.out::println);
